8aa0c330285187916b708f177058f2ea4297df0f
niko <me@nikolasgoebel.com>
2018-12-27 23:40:17 +0100

    [WIP] graph rendering

--- src/observations/core.cljs
+++ src/observations/core.cljs
@@ -3,153 +3,109 @@
    [clojure.set :as set]
    [datascript.core :as d]
    [rum.core :as rum]
-   [singult.core :refer [render]]
    [observations.graph :as graph]))
 
 (enable-console-print!)
 
-(def conn
-  (-> (d/empty-db {}) (d/conn-from-db)))
+(def schema
+  {:operator/name     {}
+   :operator/address  {:db/unique :db.unique/identity}
+   :channel/id        {:db/unique :db.unique/identity}
+   :channel/from      {}
+   :channel/to        {}
+   :channel/subgraph? {}})
 
-(defn select-static [$ id]
-  (-> (.selectAll $ id) (.data (into-array [id]))))
+(defonce conn
+  (-> (d/empty-db schema)
+      #_(d/db-with [#:operator{:name "map" :address "A"}
+                    #:operator{:name "filter" :address "B"}
+                    #:channel{:from "A" :to "B"}])
+      (d/conn-from-db)))
 
 (rum/defc graph-canvas
   < {:did-mount
      (fn [state]
        (let [[graph] (:rum/args state)
              svg     (js/d3.select "#graph-canvas")
+             g       (.selectAll svg "g")
              zoom    (doto (js/d3.zoom)
-                       (.on "zoom" #(.attr svg "transform" js/d3.event.transform)))]
-         (doto svg
-           (graph/render graph)
-           (.call zoom))
+                       (.on "zoom" #(.attr g "transform" js/d3.event.transform)))]
+         (do
+           (graph/render g graph)
+           (.call svg zoom))
          state))
      
      :did-update
      (fn [state]
        (let [[graph] (:rum/args state)
-             svg     (js/d3.select "#graph-canvas")]
-         (graph/render svg graph)
+             g       (-> (js/d3.select "#graph-canvas")
+                         (.selectAll "g"))]
+         (graph/render g graph)
          state))}
   [graph]
-  [:svg#graph-canvas {:width 960 :height 1080}])
-
-(defonce graph
-  (atom
-   (doto (js/dagreD3.graphlib.Graph. #js {:compound true})
-     (.setGraph #js {:nodesp 10 :ranksep 10})
-     (.setNode "A" #js {:labelType "html"
-                        :label     "<em>A</em>"
-                        :rx        5
-                        :ry        5})
-     (.setNode "B" #js {:labelType "html"
-                        :label     "<em>B</em>"
-                        :rx        5
-                        :ry        5})
-     (.setEdge "A" "B" #js {:label "floweth"}))))
-
-(def ^:private ^:dynamic *reactions*)
-
-(def selections
-  (atom {:enter #{} :update #{} :exit #{}}))
-
-;; @TODO key-fn
-(defn update-selections [selections diffs]
-  (->> diffs
-       (map (fn [[x d]] {:value x :diff d}))
-       (reduce
-        (fn [selections {:keys [value diff]}]
-          (let [add?      (pos? diff)
-                retract?  (neg? diff)
-                exiting?  (contains? (:exit selections) value)
-                updating? (contains? (:update selections) value)
-                entering? (contains? (:enter selections) value)
-                move      (fn [v from to]
-                            (-> selections
-                                (update from disj v)
-                                (update to conj v)))]
-            (cond
-              (and add? exiting?)      (move value :exit :update)
-              (and add? updating?)     (move value :update :enter)
-              (and add? entering?)     selections
-              (and retract? exiting?)  selections
-              (and retract? updating?) (move value :update :exit)
-              (and retract? entering?) (move value :enter :update)
-              add?                     (update selections :enter conj value)
-              retract?                 (update selections :exit conj value))))
-        (-> selections
-            (assoc :exit #{})
-            (assoc :enter #{})
-            (update :update set/union (:enter selections))))))
-
-(def differential
-  {:init
-   (fn [state props]
-     (assoc state :differential/key (random-uuid)))
-   
-   :wrap-render
-   (fn [render-fn]
-     (fn [state]
-       (binding [*reactions* (volatile! #{})]
-         (let [comp             (:rum/react-component state)
-               old-reactions    (:differential/refs state #{})
-               [dom next-state] (render-fn state)
-               new-reactions    @*reactions*
-               key              (:differential/key state)]
-           (doseq [ref old-reactions]
-             (when-not (contains? new-reactions ref)
-               (remove-watch ref key)))
-           (doseq [ref new-reactions]
-             (when-not (contains? old-reactions ref)
-               (add-watch ref key
-                          (fn [_ _ _ _]
-                            (rum/request-render comp)))))
-           [dom (assoc next-state :differential/refs new-reactions)]))))
-   
-   :will-unmountp
-   (fn [state]
-     (let [key (:differential/key state)]
-       (doseq [ref (:differential/refs state)]
-         (remove-watch ref key)))
-     (dissoc state :differential/refs :differential/key))})
-
-(defn diff-react
-  [ref]
-  (assert *reactions* "rum.core/react is only supported in conjunction with rum.core/reactive")
-  (vswap! *reactions* conj ref)
-  @ref)
+  [:svg#graph-canvas {:height 1080}
+   [:g]])
 
 (rum/defc root
-  < differential
+  < rum/reactive
   [conn]
-  (let [{:keys [enter update exit]} (diff-react conn)
-        items                       (->> (set/union enter update exit)
-                                         (sort))]
-    [:div#app-container
-     [:h1 "Timely Observations"]
-     [:ul
-      (for [i items]
-        [:li {:key   i
-              :class (cond
-                       (contains? enter i)  "enter"
-                       (contains? update i) "update"
-                       (contains? exit i)   "exit")}
-         (str "ITEM " i)])]
-     #_(graph-canvas @graph)]))
+  (let [db        (rum/react conn)
+        operators (d/q '[:find [(pull ?e q) ...]
+                         :in $ q
+                         :where [?e :operator/name _]] db graph/operator-query)
+        channels  (d/q '[:find [(pull ?e q) ...]
+                         :in $ q
+                         :where
+                         [?e :channel/from ?from]
+                         [?e :channel/to ?to]
+                         [_ :operator/address ?from]
+                         [_ :operator/address ?to]] db graph/channel-query)
+        graph     (graph/graph operators channels)]
+    [:div
+     [:nav
+      [:h1 "Timely Observations"]]
+     [:main
+      (graph-canvas graph)]]))
 
-(rum/mount (root selections) (.getElementById js/document "app-container"))
+(rum/mount (root conn) (.getElementById js/document "app-container"))
+
+(let [json->operator (fn [obj]
+                       (let [obj (.-Operate obj)]
+                         {:operator/address (js->clj (.-addr obj))
+                          :operator/name    (.-name obj)}))
+      json->channel  (fn [obj]
+                       (let [obj       (.-Channel obj)
+                             from-addr (js->clj (.-from_addr obj))
+                             to-addr   (js->clj (.-to_addr obj))]
+                         {:channel/id        (.-id obj)
+                          :channel/from      from-addr
+                          :channel/to        to-addr
+                          :channel/subgraph? (.-subgraph obj)}))
+      operate?       #(.hasOwnProperty % "Operate")
+      channel?       #(.hasOwnProperty % "Channel")
+      on-message     (fn [event]
+                       (let [updates  (-> event
+                                          (.-data)
+                                          (js/JSON.parse)
+                                          (.-updates))
+                             operates (->> updates
+                                           (filter operate?)
+                                           (map json->operator))
+                             channels (->> updates
+                                           (filter channel?)
+                                           (map json->channel))]
+                         (d/transact! conn (concat operates channels))))]
+  (doto (js/WebSocket. "ws://localhost:9000/ws/")
+    (.addEventListener "message" on-message)))
+
+
 
 (comment
+
+  (d/transact! conn [#:operator{:name "filter" :address [0 1 2 3]}])
+
+  (d/transact! conn [#:channel{:from "A" :to "C"}])
+
+  (into [] (d/datoms @conn :aevt :operator/address))
   
-  (swap! selections update-selections [[1 1] [2 1] [3 1] [4 1] [5 1]])
-
-  (swap! selections update-selections [[3 -1]])
-
-  (swap! graph (fn [g]
-                 (doto g
-                   (.setNode "C" #js {:labelType "html"
-                                      :label     "<em>C</em>"
-                                      :rx        5
-                                      :ry        5}))))
   )
--- src/observations/graph.cljs
+++ src/observations/graph.cljs
@@ -1,17 +1,47 @@
 (ns observations.graph
-  "Facilities for laying out a dataflow graph.")
+  "Facilities for laying out a dataflow graph."
+  (:require
+   [singult.core :as html]))
+
+(defn node [{:operator/keys [name address]}]
+  (let [label (html/render [:.label
+                            [:span.name name]
+                            " (" [:span.addr (str address)] ")"])]
+    #js {:labelType "html"
+         :label     label
+         :rx        5
+         :ry        5}))
+
+(defn parent [addr]
+  (when (> (count addr) 2)
+    (let [parent-addr (first addr)])))
+
+(defn edge [{:channel/keys [id from to]}]
+  #js {:label ""
+       :style ""})
+
+(def channel-query
+  '[:channel/id :channel/from :channel/to :channel/subgraph?])
+
+(def operator-query
+  '[:operator/address :operator/name])
+
+(defn graph [operators channels]
+  (let [g (doto (js/dagreD3.graphlib.Graph. #js {:compound true})
+            (.setGraph #js {:nodesp 50 :ranksep 50}))
+        _ (doseq [op operators]
+            (.setNode g (str (:operator/address op)) (node op)))
+        _ (doseq [chan channels]
+            (let [from (:channel/from chan)
+                  to   (:channel/to chan)]
+              (.setEdge g (str from) (str to) (edge chan))))]
+    g))
 
 (def sample-graph
-  (doto (js/dagreD3.graphlib.Graph. #js {:compound true})
-    (.setGraph #js {:nodesp 10 :ranksep 10})
-    (.setNode "A" #js {:labelType "html"
-                       :label     "<em>A</em>"
-                       :rx        5
-                       :ry        5})
-    (.setNode "B" #js {:labelType "html"
-                       :label     "<em>B</em>"
-                       :rx        5
-                       :ry        5})
+  (doto (graph
+         [#:operator{:name "map" :address "A"}
+          #:operator{:name "filter" :address "B"}]
+         [])
     (.setEdge "A" "B" #js {:label "floweth"})))
 
 (def render (js/dagreD3.render))
